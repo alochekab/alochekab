3acca7bfd041f64102f94b34cc3adc34bce4b75c
behnudng <behnudng@gmail.com>
2022-08-11 18:43:07 +0430

    adding a script file for the header and almost completing the navbar

---
+++ assets/scripts/header.js
@@ -0,0 +1,35 @@
+"use script";
+
+const navLi1 = document.getElementById("navbar_ul_li1");
+const navLi2 = document.getElementById("navbar_ul_li2");
+const navLi3 = document.getElementById("navbar_ul_li3");
+
+const resetBorder = function (idName) {
+  idName.style.borderBottomWidth = "";
+  idName.style.borderBottomColor = "";
+  idName.style.borderBottomStyle = "";
+};
+
+const addBottomBorder = function (liName) {
+  liName.style.borderBottomWidth = "2px";
+  liName.style.borderBottomColor = "green";
+  liName.style.borderBottomStyle = "solid";
+};
+
+navLi1.addEventListener("click", function () {
+  resetBorder(navLi2);
+  resetBorder(navLi3);
+  addBottomBorder(navLi1);
+});
+
+navLi2.addEventListener("click", function () {
+  resetBorder(navLi1);
+  resetBorder(navLi3);
+  addBottomBorder(navLi2);
+});
+
+navLi3.addEventListener("click", function () {
+  resetBorder(navLi1);
+  resetBorder(navLi2);
+  addBottomBorder(navLi3);
+});
