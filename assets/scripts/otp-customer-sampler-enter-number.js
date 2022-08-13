754c9e67c4f1d530a98f32fa91a69b39434d8c3e
behnudng <behnudng@gmail.com>
2022-08-13 16:31:29 +0430

    write some of the enter number page logic + add X icon for the exit button

--- assets/scripts/otp-customer-sampler-enter-number.js
+++ assets/scripts/otp-customer-sampler-enter-number.js
@@ -1,1 +1,11 @@
 "use strict";
+const phoneNumberInput = document.getElementById("number-panel_phone-input");
+const numberPanel = document.querySelector(".number-panel");
+const numberPanelContainer = document.querySelector(
+  ".number-panel_content-container"
+);
+
+phoneNumberInput.addEventListener("click", function () {
+  numberPanel.classList.add("fullscreen_numpanel");
+  numberPanelContainer.classList.add("fullscreen_numpanel-margin");
+});
