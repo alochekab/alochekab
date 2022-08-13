52a562b3db8c9350a9494b696a8442bcfada1677
behnudng <behnudng@gmail.com>
2022-08-13 18:44:50 +0430

    completed login page

--- assets/scripts/otp-customer-sampler-enter-number.js
+++ assets/scripts/otp-customer-sampler-enter-number.js
@@ -5,9 +5,7 @@ const numberPanelContainer = document.querySelector(
   ".number-panel_content-container"
 );
 const exitButtonDiv = document.querySelector(".number-panel_exit-button");
-
 const submitButton = document.querySelector(".number-panel_submit");
-
 const changeLanguageBtn = document.querySelector(
   ".number-panel_change-language"
 );
@@ -18,6 +16,10 @@ phoneNumberInput.addEventListener("click", function () {
   changeLanguageBtn.classList.add("display-none");
   exitButtonDiv.classList.remove("display-none");
   submitButton.classList.remove("display-none");
+  // animation
+  numberPanel.classList.remove("bring-down-ani");
+  numberPanel.classList.remove("bring-up-ani");
+  numberPanel.classList.add("bring-up-fullscreen");
 });
 
 exitButtonDiv.addEventListener("click", function () {
@@ -26,4 +28,8 @@ exitButtonDiv.addEventListener("click", function () {
   changeLanguageBtn.classList.remove("display-none");
   exitButtonDiv.classList.add("display-none");
   submitButton.classList.add("display-none");
+  // animation
+  numberPanel.classList.add("bring-down-ani");
+  numberPanel.classList.remove("bring-up-ani");
+  numberPanel.classList.remove("bring-up-fullscreen");
 });
